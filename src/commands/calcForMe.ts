5daac7cec9632681af7aa657bdea67e8b47f4945
Mara <mara-li@outlook.fr>
2023-07-09 08:21:19 +0200

    feat(calcForMe): Add bonus and malus options to calculate total

--- src/commands/calcForMe.ts
+++ src/commands/calcForMe.ts
@@ -1,5 +1,5 @@
 import {CommandInteraction, CommandInteractionOptionResolver, SlashCommandBuilder} from "discord.js";
-import {roundUp} from "../utils";
+import {logInDev, roundUp} from "../utils";
 
 export default {
 	data: new SlashCommandBuilder()
@@ -10,14 +10,46 @@ export default {
 				.setName("valeur")
 				.setDescription("Valeur de la statistique")
 				.setRequired(true)
+		)
+		.addIntegerOption((option) =>
+			option
+				.setName("bonus")
+				.setDescription("Bonus sur le jet")
+				.setRequired(false)
+		)
+		.addIntegerOption((option) =>
+			option
+				.setName("malus")
+				.setDescription("Malus sur le jet")
+				.setRequired(false)
 		),
 	async execute(interaction: CommandInteraction) {
 		if (!interaction.guild) return;
 		const option = interaction.options as CommandInteractionOptionResolver;
 		const value = option.getInteger("valeur");
+		const bonusValue = option.getInteger("bonus") ?? 0;
+		let malusValue = option.getInteger("malus") ?? 0;
+		malusValue = malusValue > 0 ? -malusValue : malusValue;
+		logInDev("malus", malusValue, "bonus", bonusValue);
 		if (value === null) return;
 		const bonus = roundUp((value - 11) / 2);
-		if (bonus < 0) await interaction.reply(`Le malus de ${value} est \`${bonus}\``);
-		else await interaction.reply(`Le bonus de ${value} est \`${bonus}\``);
+		const total = bonus + bonusValue + malusValue;
+		/** wrote function to avoid code duplication */
+		const msg = (value: number) => {
+			if (value === 0) return "";
+			return value > 0 ? ` + ${value}` : ` - ${-value}`;
+		};
+		const msgBonus = msg(bonusValue);
+		const msgMalus = msg(malusValue);
+		const resMsg = bonus < 0 ? "malus" : "bonus";
+		if (bonusValue === 0 && malusValue === 0) {
+			await interaction.reply(`La statistique \`${value}\` donne un **${resMsg}** de \`${bonus}\`.`);
+			return;
+		}
+		const msgBonusMalus = total < 0 ? "malus" : "bonus";
+		await interaction.reply(
+			`Le **${resMsg}** de la statistique \`${value}\` est de \`${bonus}\`, avec un **${msgBonusMalus}** global de \`${total}\` \`(${bonus}${msgBonus}${msgMalus})\`.`
+		);
+		return;
 	}
 };
